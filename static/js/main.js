a3786e2ca31375984aaf32178e5c19b451c610a1
Patrik Wästlund <github@wastlund.net>
2020-12-30 23:05:38 +0100

    Switch to browser-level support for lazy-loading images

--- static/js/main.js
+++ static/js/main.js
@@ -229,44 +229,4 @@
 	// Always load external inks in new tab
 	$('a[href^="//"], a[href^="http"]').not('a[href*="your-domain"]').attr('target', '_blank');
 
-	// Initialize LazyLoad
-	function logElementEvent(eventName, element) {
-		console.log(Date.now(), eventName, element.getAttribute("data-src"));
-	}
-
-	var callback_enter = function(element) {
-		logElementEvent("🔑 ENTERED", element);
-	};
-	var callback_exit = function(element) {
-		logElementEvent("🚪 EXITED", element);
-	};
-	var callback_loading = function(element) {
-		logElementEvent("⌚ LOADING", element);
-	};
-	var callback_loaded = function(element) {
-		logElementEvent("👍 LOADED", element);
-	};
-	var callback_error = function(element) {
-		logElementEvent("💀 ERROR", element);
-		element.src =
-			"https://via.placeholder.com/440x560/?text=Error+Placeholder";
-	};
-	var callback_finish = function() {
-		logElementEvent("✔️ FINISHED", document.documentElement);
-	};
-	var callback_cancel = function(element) {
-		logElementEvent("🔥 CANCEL", element);
-	};
-
-	ll = new LazyLoad({
-		// Assign the callbacks defined above
-		callback_enter: callback_enter,
-		callback_exit: callback_exit,
-		callback_cancel: callback_cancel,
-		callback_loading: callback_loading,
-		callback_loaded: callback_loaded,
-		callback_error: callback_error,
-		callback_finish: callback_finish
-	});
-
 })(jQuery);
